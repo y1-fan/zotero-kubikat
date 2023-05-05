c7f7c3f040c054836a29c6beb5778f6a9af11831
y1-fan <78839187+y1-fan@users.noreply.github.com>
2023-05-05 22:42:31 +0800

    Add files via upload

--- zotero-kubikat.py
+++ zotero-kubikat.py
@@ -8,7 +8,12 @@ f.close()
 
 text = text.replace('<<','')
 text = text.replace('>>','')
-text = re.sub('TI  - (.+)\nTI  - (.+)','TI  - \g<1>\n: \g<2>',text)
+text = re.sub('TI  - (.+)\nTI  - (.+)','T1  - \g<1>\n: \g<2>',text)
+text = text.replace('QS','CN')
+text = re.sub('TT  - (.+)\nTT  - (.+)','N1  - Translated Title: \g<1>\n: \g<2>',text)
+text = re.sub('AV  - (.+)\nAV  - (.+)','AV  - \g<1>\n; \g<2>',text)
+text = re.sub('AV  - (.+)\nAV  - (.+)\nAV  - (.+)','AV  - \g<1>\n; \g<2>\n; \g<3>',text)
+text = re.sub('AV  - (.+)\nAV  - (.+)\nAV  - (.+)\nAV  - (.+)','AV  - \g<1>\n; \g<2>\n; \g<3>\n; \g<4>',text)
 
 f = open(str,"w",encoding = 'UTF-8')
 f.write(text)
