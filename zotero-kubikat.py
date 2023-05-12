fe129b6fe72911325b68985513700bbec9e49e56
y1-fan <78839187+y1-fan@users.noreply.github.com>
2023-05-12 14:32:14 +0800

    Add files via upload

--- zotero-kubikat.py
+++ zotero-kubikat.py
@@ -9,7 +9,6 @@ f.close()
 text = text.replace('<<','')
 text = text.replace('>>','')
 text = re.sub('TI  - (.+)\nTI  - (.+)','TI  - \g<1>\n: \g<2>',text)
-text = text.replace('QS','CN')
 text = re.sub('TT  - (.+)\nTT  - (.+)','N1  - Translated Title: \g<1> : \g<2>',text)
 
 text = re.sub('AV  - (.+)\nAV  - (.+)\nAV  - (.+)\nAV  - (.+)','AV  - \g<1>\n; \g<2>\n; \g<3>\n; \g<4>',text)
@@ -23,6 +22,10 @@ text = re.sub('PB  - (.+)\nPB  - (.+)\nPB  - (.+)','PB  - \g<1>\n; \g<2>\n; \g<3
 text = re.sub('PB  - (.+)\nPB  - (.+)','PB  - \g<1>\n; \g<2>',text)
 
 text = text.replace('UR','L2')
+
+text = re.sub('SP  - (.+)\nQS  - (.+)','QS  - \g<2>',text)
+text = re.sub('SP  - (.+)\n((.+)\n+)QS  - (.+)','\g<1>QS  - \g<4>',text)
+text = text.replace('QS','SP')
 
 f = open(str,"w",encoding = 'UTF-8')
 f.write(text)
